0cec3b212d6a68af303dd8d8db2e23bcdb638509
Abayomi Aluko <franicons@gmail.com>
2022-12-15 00:55:06 +0100

    fix: add-loader

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,26 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+  {
+    path: "/:catachAll(.*)*",
+    redirect: { name: "home" },
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About,
-    },
-  ],
+  routes,
 });
 
 export default router;
